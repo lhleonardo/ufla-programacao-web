a6a0f512938e747f3a9338e5a54431c9daaa21cd
Leonardo Braz <lhleonardo05@gmail.com>
2020-11-06 14:42:24 -0400

    Valores estáticos

--- reo02-catalogo-carros/src/providers/static.ts
+++ reo02-catalogo-carros/src/providers/static.ts
@@ -11,74 +11,18 @@ export const STATIC_CARS: Car[] = [
   },
   {
     id: uuid(),
-    brand: "Toyota",
-    model: "Corolla",
+    brand: "Ford",
+    model: "Ford Ka",
     year: 2020,
     imageUrl:
-      "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/images/car-reviews/first-drives/legacy/large-2479-s-classsaloon.jpg?itok=QTxMln2k",
+      "https://s2.glbimg.com/9XufU2Gc0q9D_X74qc0kmk0aZNw=/620x413/e.glbimg.com/og/ed/f/original/2015/12/01/novo-ford-ka.jpg",
   },
   {
     id: uuid(),
-    brand: "Toyota",
-    model: "Corolla",
+    brand: "Honda",
+    model: "Honda Civic",
     year: 2020,
     imageUrl:
-      "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/images/car-reviews/first-drives/legacy/large-2479-s-classsaloon.jpg?itok=QTxMln2k",
-  },
-  {
-    id: uuid(),
-    brand: "Toyota",
-    model: "Corolla",
-    year: 2020,
-    imageUrl:
-      "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/images/car-reviews/first-drives/legacy/large-2479-s-classsaloon.jpg?itok=QTxMln2k",
-  },
-  {
-    id: uuid(),
-    brand: "Toyota",
-    model: "Corolla",
-    year: 2020,
-    imageUrl:
-      "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/images/car-reviews/first-drives/legacy/large-2479-s-classsaloon.jpg?itok=QTxMln2k",
-  },
-  {
-    id: uuid(),
-    brand: "Toyota",
-    model: "Corolla",
-    year: 2020,
-    imageUrl:
-      "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/images/car-reviews/first-drives/legacy/large-2479-s-classsaloon.jpg?itok=QTxMln2k",
-  },
-  {
-    id: uuid(),
-    brand: "Toyota",
-    model: "Corolla",
-    year: 2020,
-    imageUrl:
-      "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/images/car-reviews/first-drives/legacy/large-2479-s-classsaloon.jpg?itok=QTxMln2k",
-  },
-  {
-    id: uuid(),
-    brand: "Toyota",
-    model: "Corolla",
-    year: 2020,
-    imageUrl:
-      "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/images/car-reviews/first-drives/legacy/large-2479-s-classsaloon.jpg?itok=QTxMln2k",
-  },
-  {
-    id: uuid(),
-    brand: "Toyota",
-    model: "Corolla",
-    year: 2020,
-    imageUrl:
-      "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/images/car-reviews/first-drives/legacy/large-2479-s-classsaloon.jpg?itok=QTxMln2k",
-  },
-  {
-    id: uuid(),
-    brand: "Toyota",
-    model: "Corolla",
-    year: 2020,
-    imageUrl:
-      "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/images/car-reviews/first-drives/legacy/large-2479-s-classsaloon.jpg?itok=QTxMln2k",
+      "https://1.bp.blogspot.com/-ALo53OFsCJc/Xv5hNr1sfpI/AAAAAAAAfko/UniQjgxT8NocKIsVD065-Lra2250HNGIQCLcBGAsYHQ/s1600/Honda-Civic-Si-2021%2B%252834%2529.jpg",
   },
 ];
